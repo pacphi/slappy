f886f151b4a837db9455f9afd3204101dfce6140
Chris Phillipson <christopher.a.phillipson@gmail.com>
2025-10-27 11:13:47 -0700

    fix: add overrides as workaround to address https://github.com/vuejs/pinia/issues/3057

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
   future: { compatibilityVersion: 4 },
   modules: ['@nuxt/ui', '@pinia/nuxt', '@pinia/colada-nuxt', '@nuxt/eslint'],
   css: ['~/assets/css/main.css'],
